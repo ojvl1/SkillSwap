2f65c7d1289bfe8991abe4385bca0f7d525c46df
ojvl1 <antompietriosvaldo@gmail.com>
2025-04-09 11:11:19 -0400

    change

--- SkillSwap/src/App.jsx
+++ SkillSwap/src/App.jsx
@@ -1,16 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
 
-function App() {
-  return (
-    <>
-      
-    </>
-  )
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
   </StrictMode>,
 )
