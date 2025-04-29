50d401496b11d9e0526620b97eed87d49debb236
ojvl1 <antompietriosvaldo@gmail.com>
2025-04-29 16:50:54 -0400

    .

--- SkillSwap/src/components/Dashboard/Career List/careerList.jsx
+++ SkillSwap/src/components/Dashboard/Career List/careerList.jsx
@@ -13,6 +13,10 @@ function careeList() {
     "Cars Mechanic",
     "Electronics"
   ];
+
+  return(
+    
+  );
 }
 
 export default careeList;
--- SkillSwap/src/components/Dashboard/Dashboard.jsx
+++ SkillSwap/src/components/Dashboard/Dashboard.jsx
@@ -2,26 +2,9 @@ import React from "react";
 import "./Dashboard.css"
 
 function Dashboard() {
-  const career_list = [
-    "Programmer",
-    "Sign Laguage",
-    "Language",
-    "Agriculture",
-    "Computer Repair",
-    "Handy Man",
-    "Instruments",
-    "Sing",
-    "Cars Mechanic",
-    "Electronics"
-  ];
-
   return(
     <>
-      <div className="find-careers-box">
-        <ul>
-          <li>{career_list}</li>
-        </ul>
-      </div>
+      
     </>
   );
 }
