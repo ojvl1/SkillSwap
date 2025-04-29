affb086b814678e24cfab4e26ae0465c9f4b5a02
ojvl1 <carlosantompietri175@gmail.com>
2025-04-29 13:24:52 -0400

    .

--- SkillSwap/src/App.jsx
+++ SkillSwap/src/App.jsx
@@ -1,12 +1,15 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import LogIn from './components/LogIn/LogIn'
 import SignIn from './components/SignIn/SignIn'
 import Careers from './components/Careers/Careers'
 import Background from './components/Background/Background'
+import Dashboard from './components/Dashboard/Dashboard'
 import './App.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Background />
+    
   </StrictMode>
 )
