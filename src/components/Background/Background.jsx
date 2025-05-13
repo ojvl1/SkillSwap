aab99736ef743542d8861d25cba5696deba34bfe
ojvl1 <carlosantompietri175@gmail.com>
2025-05-13 09:04:50 -0400

    make Upload and styling it

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom"
 import Logo from "../../assets/SkillSwap-Logo.png"
 import Dashboard from "../Dashboard/Dashboard.jsx"
+import { FileUpload } from "primereact/fileupload";
 import "./Background.css"
 
 function Background() {
@@ -18,10 +18,26 @@ function Background() {
           <input placeholder="Last Name" type="text" />
           <input placeholder="Age" type="number" name="Age" />
         </div>
-        <div className="dragdrop">
-          <DragDrop />
+        <div className="upload">
+          <FileUpload
+          name="demo[]"
+          url={'/api/upload'}
+          accept="pdf/*"
+          maxFileSize={1000000}
+          chooseOptions={{ label: 'Choose', icon: 'pi pi-plus', className: 'custom-choose-btn' }}
+          uploadOptions={{ label: 'Upload', icon: 'pi pi-upload', className: 'custom-upload-btn' }}
+          cancelOptions={{ label: 'Cancel', icon: 'pi pi-times', className: 'custom-cancel-btn' }}
+          headerTemplate={() => (
+          <div className="custom-header">
+            <p className="m-0">Drag and drop files to here to upload.</p>
+          <div className="custom-buttons">
+            <span className="p-button p-fileupload-choose custom-choose-btn">Choose</span>
+            <span className="p-button p-fileupload-upload custom-upload-btn">Upload</span>
+            <span className="p-button p-fileupload-cancel custom-cancel-btn">Cancel</span>
+          </div>
         </div>
-          <Link to={Dashboard}>Next</Link>
+          )} />
+        </div>
       </div>
     </div>
     </>
