875804c52dc54c22c9c08cafcc9ad692ac2994d3
ojvl1 <carlosantompietri175@gmail.com>
2025-05-15 17:04:29 -0400

    Make style

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import SkillSwapLogo from "../../assets/SkillSwap-Logo.png"
+import "./LogIn.css";
 
 function LogIn() {
-    return (
-      <>
-      <div>
-        <div>
-          <img src={SkillSwapLogo} alt="SkillSwap Logo" />
-        </div>
-        <div>
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-form">
+          <div className="logo-container">
+            <img src={SkillSwapLogo} alt="SkillSwap Logo" />
+          </div>
           <div>
             <h1>Log In</h1>
           </div>
-          <div>
+          <div className="form-inputs">
             <label htmlFor="email">Enter your email</label>
             <input type="email" name="email" className="email" />
-            <label htmlFor="pasword">Enter your pasword</label>
+            <label htmlFor="password">Enter your password</label>
             <input type="password" name="password" className="password" />
-            <button>Enter</button>
-            <div>
-              <p>Your don't have an account? <a href="./components/SignIn/SignIn.jsx">Sing In</a></p>
+            <button className="login-button">Enter</button>
+            <div className="signup-link">
+              <p>You don't have an account? <a href="./components/SignIn/SignIn.jsx">Sign In</a></p>
             </div>
           </div>
         </div>
       </div>
     </>
-    )
+  );
 }
 
 export default LogIn;
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,31 +1,34 @@
 import React from "react";
-import SkillSwapLogo from "../../assets/SkillSwap-Logo.png"
+import SkillSwapLogo from "../../assets/SkillSwap-Logo.png";
+import "./SignIn.css";
 
 function SignIn() {
-  return(
+  return (
     <>
-      <div>
-        <div>
-          <img src={SkillSwapLogo} alt="Skill Swap Logo" />
-        </div>
-        <div>
-          <div>
-            <p>Create an Account</p>
+      <div className="signin-container">
+        <div className="signin-form">
+          <div className="logo-container">
+            <img src={SkillSwapLogo} alt="SkillSwap Logo" />
           </div>
           <div>
-            <label htmlFor="Email">Enter a email</label>
-            <input type="email" name="email" />
+            <h1>Create an Account</h1>
+          </div>
+          <div className="form-inputs">
+            <label htmlFor="email">Enter a email</label>
+            <input type="email" name="email" className="email" />
             <label htmlFor="password">Enter a password</label>
-            <input type="password" name="password" />
-            <label htmlFor="confirm password">Confirm password</label>
-            <input type="password" name="confirm password" />
-            <button>Enter</button>
-            <p>Have an account? <a href="./components/LogIn/LogIn.jsx">LogIn</a></p>
+            <input type="password" name="password" className="password" />
+            <label htmlFor="confirmPassword">Confirm password</label>
+            <input type="password" name="confirmPassword" className="password" />
+            <button className="signin-button">Enter</button>
+            <div className="login-link">
+              <p>Have an account? <a href="./components/LogIn/LogIn.jsx">Log In</a></p>
+            </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default SignIn;
